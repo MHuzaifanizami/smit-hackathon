0ed3723f12d597f04c970c479475b3b209f68fb2
M Huzaifa Nizami <mhuzaifanizami@gmail.com>
2025-01-25 14:35:01 +0500

    update

--- Backend/index.js
+++ Backend/index.js
@@ -12,10 +12,12 @@ import jwt from "jsonwebtoken";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: '*', 
-    credentials: true, 
-  }));
+// app.use(cors({
+//     origin: '*', 
+//     credentials: true, 
+//   }));
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
